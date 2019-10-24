e42cccb28d9754d94af3725ab45e4a02af1a456d
Guillem Moreso <guillem.moreso@gmail.com>
2019-10-24 15:54:52 +0200

    WIP courts seed

--- bin/seed.js
+++ bin/seed.js
@@ -23,24 +23,52 @@ const clubs = [
     description: 'Tenis barcelona...',
     city: 'Barcelona',
     price: 12,
+    courts: ['5db1a7c8ba2eed10af5b7115'],
   },
   {
     name: 'Club tenis la Salut',
     description: 'Tenis la Salut...',
     city: 'Barcelona',
     price: 10,
+    courts: ['5db1a7c8ba2eed10af5b7116'],
   },
   {
     name: 'Club tenis Laietà',
     description: 'Tenis Laietà...',
     city: 'Barcelona',
     price: 9,
+    courts: ['5db1a7c8ba2eed10af5b7117'],
+  },
+];
+
+const courts = [
+  {
+    courtName: 'Barcelona-1',
+    timetableArray: ['1', '2', '3'],
+  },
+  {
+    courtName: 'Salut-1',
+    timetableArray: ['1', '2', '3'],
+  },
+  {
+    courtName: 'Laietà-1',
+    timetableArray: ['1', '2', '3'],
   },
 ];
 
 Club.create(clubs)
   .then(club => {
     console.log('inserted club ', club);
+    mongoose.connection.close();
+  })
+  .catch(err => {
+    console.log(err);
+    mongoose.connection.close();
+  });
+
+Court.create(courts)
+  .then(court => {
+    console.log('inserted court ', court);
     mongoose.connection.close();
   })
   .catch(err => {
