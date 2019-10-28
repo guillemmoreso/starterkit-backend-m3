38d08b02dc9d2a15991cbab02069f937102144de
Guillem Moreso <guillem.moreso@gmail.com>
2019-10-28 22:32:52 +0100

    bookings route updated

--- routes/bookings.js
+++ routes/bookings.js
@@ -7,7 +7,7 @@ const { checkIfLoggedIn } = require('../middlewares/index');
 /* GET filtered clubs listing. */
 router.get('/', async (req, res, next) => {
   try {
-    const bookings = await Booking.find().populate('courts users clubs');
+    const bookings = await Booking.find().populate('court user club');
     console.log('bookings: ', bookings);
     res.json(bookings);
   } catch (error) {
