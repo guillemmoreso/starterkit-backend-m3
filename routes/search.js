122abe361a5501fa955d496bbb459e1474fd3e87
Guillem Moreso <guillem.moreso@gmail.com>
2019-10-31 13:42:30 +0100

    WIP filtering datapicker

--- routes/search.js
+++ routes/search.js
@@ -1,37 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const Club = require('../models/Club');
+const Booking = require('../models/Booking');
 
 const { checkIfLoggedIn } = require('../middlewares/index');
 
 /* GET filtered clubs listing. */
-router.get('/', async (req, res, next) => {
-  const { date } = req.body;
+
+router.post('/', async (req, res, next) => {
+  const { date, searchStartingHour } = req.body;
   try {
-    const clubs = await Club.find({
-      //   openingHours: { $gt: 23 },
-      openingHours: { $gt: new Date().getHours() },
-    }).populate('courts');
-    if (clubs.length === 0) {
-      //COMO PUEDO MANDAR UN MENSAJE AQUÍ? DEBO CONSTRUIR UN OBJETO?
-      res.json(clubs);
-    } else {
-      //   console.log('CLUBFILTER', clubs);
-      res.json(clubs);
-    }
+    const clubsStartingHour = await Club.find({
+      openingHours: { $eq: searchStartingHour },
+    });
+    // console.log('date', date);
+
+    const searchMatchInBooking = await Booking.find({
+      // startingHour: { $eq: searchStartingHour },
+      day: { $eq: date },
+    }).populate('club');
+
+    console.log('searchMatchInBooking', searchMatchInBooking);
+
+    return res.json(clubsStartingHour);
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/', async (req, res, next) => {
-  const { startingHour } = req.body;
+router.get('/', async (req, res, next) => {
+  const { date, startingHour } = req.body;
   try {
     const clubs = await Club.find({
-      openingHours: { $eq: startingHour },
-    });
-
-    return res.json(clubs);
+      openingHours: { $gt: new Date().getHours() },
+    }).populate('courts');
+    if (clubs.length === 0) {
+      res.json(clubs);
+    } else {
+      //   console.log('CLUBFILTER', clubs);
+      res.json(clubs);
+    }
   } catch (error) {
     next(error);
   }
