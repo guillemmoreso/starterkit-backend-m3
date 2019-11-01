ed29cb719e0e9d25449fa2a9de40e6534acd749c
Guillem Moreso <guillem.moreso@gmail.com>
2019-11-01 17:10:31 +0100

    WIP transfer states datpicker

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Booking = require('../models/Booking');
+const Club = require('../models/Club');
 
 const { checkIfLoggedIn } = require('../middlewares/index');
 
@@ -10,6 +11,20 @@ router.get('/', async (req, res, next) => {
     const bookings = await Booking.find().populate('court user club');
     console.log('bookings: ', bookings);
     res.json(bookings);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:clubId', async (req, res, next) => {
+  const { clubId } = req.params;
+  try {
+    const club = await Club.findById(clubId).populate('courts');
+    if (club) {
+      res.json(club);
+    } else {
+      res.json({});
+    }
   } catch (error) {
     next(error);
   }
